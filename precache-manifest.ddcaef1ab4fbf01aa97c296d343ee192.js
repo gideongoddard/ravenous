eac5cc0e14d33890a0200c7bc07ecde7039e2c6c
Gideon Goddard <goddard.gcw@gmail.com>
2020-01-20 16:45:36 +0000

    Updates

---
+++ precache-manifest.ddcaef1ab4fbf01aa97c296d343ee192.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757c262597d5c69ee6b2626480310ebb",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "30ae3a6396dc3c170b5b",
+    "url": "/ravenous/static/css/main.9acfa5fa.chunk.css"
+  },
+  {
+    "revision": "347b46869852e18627d2",
+    "url": "/ravenous/static/js/2.0c1cd056.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ravenous/static/js/2.0c1cd056.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30ae3a6396dc3c170b5b",
+    "url": "/ravenous/static/js/main.4099398b.chunk.js"
+  },
+  {
+    "revision": "b3aa5ddacef923a6b3af",
+    "url": "/ravenous/static/js/runtime-main.40d8a6de.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
